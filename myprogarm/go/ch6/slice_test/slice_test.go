14fa9bfba3f91cfc709908dcc8ddebfd2b116ac9
ray4ray <lyyygz@163.com>
2019-08-17 18:48:57 +0800

    add go test

---
+++ myprogarm/go/ch6/slice_test/slice_test.go
@@ -0,0 +1,17 @@
+package slice_test
+
+import "testing"
+
+func TestSlienceInit(t *testing.T) {
+	var s0 []int
+	t.Log(len(s0), cap(s0))
+	s0 = append(s0, 1)
+	t.Log(len(s0), cap(s0))
+
+	s1 := []int{1, 2, 3, 4}
+	t.Log(len(s1), cap(s1))
+
+	s2 := make([]int, 3, 5)
+	t.Log(len(s2), cap(s2))
+	t.Log(s2[0], s2[1], s2[3], s2[4])
+}
