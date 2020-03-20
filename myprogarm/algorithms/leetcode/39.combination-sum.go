fe98a4cfc39430eb8f1eb2ec0740c9ab072e1fbc
ray4ray <lyyygz@163.com>
2020-03-21 01:40:06 +0800

    Add go version

--- myprogarm/algorithms/leetcode/39.combination-sum.go
+++ myprogarm/algorithms/leetcode/39.combination-sum.go
@@ -1,3 +1,5 @@
+import "sort"
+
 /*
  * @lc app=leetcode id=39 lang=golang
  *
@@ -6,7 +8,26 @@
 
 // @lc code=start
 func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	res := [][]int{}
+	dfs(candidates, nil, target, 0, &res)
+	return res
+}
 
+func dfs(candidates, nums []int, target, left int, res *[][]int) {
+	if target == 0 {
+		tmp := make([]int, len(nums))
+		copy(tmp, nums)
+		*res = append(*res, tmp)
+		return
+	}
+
+	for i := left; i < len(candidates); i++ {
+		if target < candidates[i] {
+			return
+		}
+		dfs(candidates, append(nums, candidates[i]), target-candidates[i], i, res)
+	}
 }
 
 // @lc code=end
