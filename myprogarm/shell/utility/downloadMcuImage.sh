f186af352dbbbf821cd8e72c9d9ad1b488bb23c5
gsuo <guanghui.suo@nokia-sbell.com>
2020-04-08 13:28:34 +0800

    update shell and k8s go

--- myprogarm/shell/utility/downloadMcuImage.sh
+++ myprogarm/shell/utility/downloadMcuImage.sh
@@ -2,6 +2,7 @@
 
 regis="$1"
 tag="$2"
+xptag="$3"
 
 
 echo "################# delete current mcu & test images ##############################"
@@ -26,25 +27,51 @@ fi
 echo "#################### pull and push mcu & test images ################################"
 
 if [[ "$regis" = offical* ]]; then
-    sudo docker pull rcp-docker-containers-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu:$tag
+    MCUImage=rcp-docker-containers-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu:$tag
 else
-    sudo docker pull rcp-docker-testing-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu:$tag
+    MCUImage=rcp-docker-testing-local.artifactory-espoo2.int.net.nokia.com/ccs-mcu/ccs-mcu:$tag
 fi
-newMcuImage=$(sudo docker images | grep 'ccs-mcu ' | awk  -F ' ' '{print $3}')
-sudo docker tag $newMcuImage  registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu:$tag
+sudo docker pull $MCUImage
+sudo docker tag $MCUImage registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu:$tag
 sudo docker push registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu:$tag
 
 echo "deregister mcu image from docker registery"
+sudo docker rmi $MCUImage
+sudo docker images | grep 'ccs-mcu '
+
+
 if [[ "$regis" = offical* ]]; then
-    sudo docker rmi rcp-docker-containers-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu:$tag
+    XPIFImage=rcp-docker-containers-local.esisoj70.emea.nsn-net.net/xpif/xpif:$xptag
 else
-    sudo docker rmi rcp-docker-testing-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu:$tag
+    XPIFImage=rcp-docker-testing-local.artifactory-espoo2.int.net.nokia.com/xpif/xpif:$xptag
+fi
+sudo docker pull $XPIFImage
+sudo docker tag $XPIFImage registry.kube-system.svc.nokia.net:5555/xpif/xpif:$xptag
+sudo docker push registry.kube-system.svc.nokia.net:5555/xpif/xpif:$xptag
+
+echo "deregister mcu image from docker registery"
+sudo docker rmi $XPIFImage
+sudo docker images | grep 'xpif '
+
+
+if [[ "$regis" = offical* ]]; then
+    echo "for offical image, do nothing for test image"
+else
+    MCUTESTImage=rcp-docker-testing-local./rtifactory-espoo2.int.net.nokia.com/ccs-mcu/ccs-mcu-test:$tag
+    sudo docker pull $MCUTESTImage
+    sudo docker tag $MCUTESTImage registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu-test:$tag
+    sudo docker push registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu-test:$tag
+
+    echo "deregister mcu test image from docker registery"
+    sudo docker rmi $MCUTESTImage
 fi
 
-sudo docker pull rcp-docker-testing-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu-test:$tag
-newMcuTestImage=$(sudo docker images | grep 'ccs-mcu-test ' | awk  -F ' ' '{print $3}')
-sudo docker tag $newMcuTestImage registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu-test:$tag
-sudo docker push registry.kube-system.svc.nokia.net:5555/ccs-mcu/ccs-mcu-test:$tag
 
-echo "deregister mcu test image from docker registery"
-sudo docker rmi rcp-docker-testing-local.esisoj70.emea.nsn-net.net/ccs-mcu/ccs-mcu-test:$tag
+currentLWSDImage=$(sudo docker images | grep 'docker-lwsd ' | awk  -F ' ' '{print $3}')
+if [ -z "$currentLWSDImage" ]; then
+    echo "######### current no lwsd image in local, download lwsd image ####################"
+    LWSDImage=rcp-docker-containers-local.esisoj70.emea.nsn-net.net/lwsd/docker-lwsd:1.0.1
+    sudo docker pull $LWSDImage
+    sudo docker tag $LWSDImage registry.kube-system.svc.nokia.net:5555/lwsd/docker-lwsd:1.0.1
+    sudo docker rmi $LWSDImage
+fi
